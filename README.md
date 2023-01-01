fc810c474dd2fe2b077d4d5dd64675c930cd41ea
Rob Lowcock <rob.lowcock@gmail.com>
2023-01-01 20:13:12 +0000

    Fix formatting

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Rob's 2023 Blog
 
-This is a repo for me to experiment with Deno, TypeScript and React. There will be a _lot_ of messy code in here, and plenty of examples of what not to do, but it's very much designed for experimenting.
+This is a repo for me to experiment with Deno, TypeScript and React. There will
+be a _lot_ of messy code in here, and plenty of examples of what not to do, but
+it's very much designed for experimenting.
 
-For more chaos as I go along, follow me on [@robl@hachyderm.io](https://hachyderm.io/@robl).
+For more chaos as I go along, follow me on
+[@robl@hachyderm.io](https://hachyderm.io/@robl).
--- main.ts
+++ main.ts
@@ -1,24 +1,25 @@
 import express from "npm:express@4.18.2";
 import { ApolloServer } from "npm:@apollo/server@4.3.0";
-import { ApolloServerPluginLandingPageDisabled } from 'npm:@apollo/server/plugin/disabled';
+import { ApolloServerPluginLandingPageDisabled } from "npm:@apollo/server/plugin/disabled";
 import { ApolloServerPluginLandingPageLocalDefault } from "npm:@apollo/server/plugin/landingPage/default";
 import { expressMiddleware } from "npm:@apollo/server/express4";
-import { typeDefs } from "./schema.ts"
-import { resolvers } from "./resolvers.ts"
-import cors from "npm:cors"
-import bodyParser from "npm:body-parser"
+import { typeDefs } from "./schema.ts";
+import { dinosaurResolver } from "./resolvers/dinosaurs.ts";
+import cors from "npm:cors";
+import bodyParser from "npm:body-parser";
 import "https://deno.land/x/dotenv@v3.2.0/load.ts";
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
+  resolvers: [dinosaurResolver],
   plugins: [
-    Deno.env.get("NODE_ENV") === 'production' ? ApolloServerPluginLandingPageDisabled()
-      : ApolloServerPluginLandingPageLocalDefault()
-  ]
+    Deno.env.get("NODE_ENV") === "production"
+      ? ApolloServerPluginLandingPageDisabled()
+      : ApolloServerPluginLandingPageLocalDefault(),
+  ],
 });
 
-const app : express.app = express();
+const app: express.app = express();
 
 await server.start();
 
@@ -26,10 +27,10 @@ app.use(
   "/graphql",
   cors<cors.CorsRequest>(),
   bodyParser.json(),
-  expressMiddleware(server)
+  expressMiddleware(server),
 );
 
-app.get("/", (_req: express.request, res: express.res) : void => {
+app.get("/", (_req: express.request, res: express.res): void => {
   res.send("Welcome to the Dinosaur API!");
 });
 
